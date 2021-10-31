447443055e3f4c2191c80f185fbee9b90771db3a
AlessandroGenerale17 <a.generale@student.vu.nl>
2021-10-31 15:14:21 +0100

    add bulk and single delete route for snippets

--- routers/snippet.js
+++ routers/snippet.js
@@ -25,11 +25,20 @@ router
     })
     .delete(async (req, res, next) => {
         try {
-            if (req.body.snippets.length > 1) {
-                return res.status(200);
+            const idsArray = req.headers.data
+                .split(',')
+                .map((id) => parseInt(id));
+            if (idsArray.length <= 1) {
+                return res
+                    .status(400)
+                    .send(
+                        'Bad request, this route deletes many items, only one was passed. Check other route /snippet/:id'
+                    );
             }
-            return res.status(200).send('I received');
-            // Model.destroy({ where: { id: [1,2,3,4] }})
+            await Snippet.destroy({
+                where: { id: idsArray }
+            });
+            return res.status(200).send('Deleted snippets');
         } catch (err) {
             next(err);
         }
@@ -49,6 +58,11 @@ router
     })
     .delete(async (req, res, next) => {
         try {
+            const id = parseInt(req.params.id);
+            const snippet = await Snippet.findByPk(id);
+            if (!snippet) return res.status(404).send('Snippet not found');
+            snippet.destroy();
+            return res.status(200).send('Snippet deleted');
         } catch (err) {
             next(err);
         }
